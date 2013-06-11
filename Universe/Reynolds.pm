39649585efff1b15cb43656a0482f34c3c9124b3
Mouq <alexmoquin@gmail.com>
2013-06-11 06:10:16 -0400

    Added tests and improved the core

--- Universe/Reynolds.pm
+++ Universe/Reynolds.pm
@@ -15,54 +15,64 @@ use Math::Trig qw(pi);
 
 use Math::Vector::Real::Random;
 
-# A sphere is an object
-# with a position V(x,y,z)
-# and constant radius:
-has sphere_r => ( is => 'ro', isa => 'Num', default => 2.767e-18 );
-
-# Size of simulation
-has universe_r => ( is => 'ro', isa => 'Num', default => 1e-17 );    #5e-17
-
-# Comfortable kd-Tree home
-# for our new round family
-has universe => ( is => 'rw' );
+has grain_radius => ( is => 'ro', isa => 'Num', default => 2.767e-18 );
+has radius       => ( is => 'rw', isa => 'Num', default => 1e-17 );
+has positions    => ( is => 'rw', default => sub { Math::Vector::Real::kdTree->new } );
+has velocities   => ( is => 'rw', default => sub { [] });
 
 sub in_container {
-    my $self   = shift;
-    my $sphere = shift;
+    my $self  = shift;
+    my $grain = shift;
 
     # Container is just a sphere
     # for now
 
-    abs($sphere) + $self->sphere_r > $self->universe_r ? 0 : 1;
+    abs($grain) + $self->grain_radius > $self->radius ? 0 : 1;
+}
+
+sub intersection_at {
+    my $self = shift;
+    my $p = shift;
+    carp "Undefined point" and return unless defined($p);
+    my $n = $self->positions->find_nearest_neighbor($p) or return 0;
+    $n->[1] < 2 * $self->grain_radius;
 }
 
 sub fill_random {
     my $self = shift;
     my $percent_fill = shift // 1 - 1e-5;
-    $self->universe = Math::Vector::Real::kdTree->new(
+    $self->positions(Math::Vector::Real::kdTree->new(
         Math::Vector::Real->random_in_sphere(
-            3, $self->universe_r - $self->sphere_r
+            3, $self->radius - $self->grain_radius
         )
-    );
+    ));
 
-    while ( volume( $self->sphere_r ) * ( $self->universe->size + 1 ) <
-        volume( $self->universe_r ) * $self->percent_fill )
+    while ( volume( $self->grain_radius ) * ( $self->positions->size + 1 ) <
+        volume( $self->radius ) * $percent_fill )
     {
         my $tmp = Math::Vector::Real->random_in_sphere( 3,
-            $self->universe_r - $self->sphere_r );
-        $self->universe->insert($tmp)
-          unless [ $self->universe->find_nearest_neighbor($tmp) ]->[1] <
-          2 * $self->sphere_r;
-    }
+            $self->radius - $self->grain_radius );
 
-    1;
+        $self->insert([$tmp,V(0,0,0)])
+          unless $self->intersection_at($tmp);
+    }
 }
 
 sub step {
     my $self    = shift;
     my $step_by = shift;
     ...;
+}
+
+sub insert {
+    my $self = shift;
+    for (@_){
+        my ($p,$v) = @$_; 
+        carp "Grain attempted to be inserted out of bounds" && next if $self->in_container($p);
+        carp "Grain attempted to be placed intersecting another grain" && next if $self->intersection_at($p);
+        my $i = $self->positions->insert($p);
+        $self->velocities->[$i] = $v;
+    }
 }
 
 sub next_collision {
@@ -73,9 +83,7 @@ sub probably_next_collision {
     ...;
 }
 
-sub volume {
-    return 4 / 3 * pi * ( shift()**3 );
-}
+sub volume { 4 / 3 * pi * ( shift()**3 ) }
 
 1;
 
---
+++ t/basic.t
@@ -0,0 +1,28 @@
+use Test::More tests => 4;
+use Test::Exception;
+
+use Universe::Reynolds;
+
+my $u = Universe::Reynolds->new;
+
+isa_ok( $u, 'Universe::Reynolds' );
+
+can_ok( $u, qw(step next_collision) );
+
+$u = Universe::Reynolds->new( grain_radius => 1, radius => 1 );
+$u->fill_random(1);
+
+is( $u->positions->size, 1 );
+
+$u = Universe::Reynolds->new( grain_radius => 1, radius => 2 );
+eval {$u->insert([ [0,0,1], [0,0,0] ], [ [0,0,-1], [0,0,0] ]) };
+
+is( $u->positions->size, 2);
+__END__
+$u = Universe::Reynolds->new( grain_radius => 1, radius => 2 );
+eval{ $u->insert([ [0,0,1], [0,0,0] ], [ [0,0,-1.5], [0,0,0] ]) };
+
+like( $@, qr[out of bounds]);
+
+is( $u->positions->size, 1);
+
